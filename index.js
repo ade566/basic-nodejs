8b90fc73c98c999f3d57428ff888902fea69ef95
Ade Rohayat <muhamadaderohayat122@gmail.com>
2021-12-05 19:01:08 +0700

    Parsing data

--- index.js
+++ index.js
@@ -4,16 +4,28 @@ const app = express()
 const port = 3000
 
 const sayHello = say.hello('Muhamad Ade Rohayat')
+const users = [
+  { id: 1, 'name': 'Muhamad Ade Rohayat', 'job': 'Programmer' },
+  { id: 2, 'name': 'Mila Safitri', 'job': 'Programmer' },
+  { id: 3, 'name': 'Ilham Apriyadi', 'job': 'Desainer UI/UX' }
+]
 
-app.set('views', './views')
+app.use(express.static(__dirname + '/public'));
+app.set('views', './public/views')
 app.set('view engine', 'ejs')
 
+
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { users })
 })
 
-app.get('/user/:name', (req, res) => {
-  res.send(say.hello(req.params.name))
+app.get('/user/:id', (req, res) => {
+  const index = users.findIndex(check)
+  function check(index) {
+    return index.id == req.params.id;
+  }
+  let user = users[index];
+  res.render('user', { user })
 })
 
 app.listen(port, () => {
